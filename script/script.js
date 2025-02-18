788f5a6869b40034c7a820e9f4d449ab22f11804
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-18 11:02:22 -0800

    Create playGame, getComputerChoice, getRandomInt()

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
+function playGame(choice) {
+  
+}
 
+function getComputerChoice() {
+  const randomInt = getRandomInt();
+  
+  if (randomInt === 0) {
+    return "rock";
+  }
+  
+  else if (randomInt === 1) {
+    return "paper";
+  }
+  
+  else {
+    return "scissors";
+  }
+}
+
+function getRandomInt() {
+  // Will return a value between 0 and 2
+  return Math.floor(Math.random() * 3);
+}
