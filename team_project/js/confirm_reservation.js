78bc63c0b2bce4eb0c216cdfee3e08387b5949d5
aatiqa234 <aatiqa.imran@bellevuecollege.edu>
2025-03-06 05:56:23 -0800

    Update confirm_reservation.js

--- team_project/js/confirm_reservation.js
+++ team_project/js/confirm_reservation.js
@@ -92,3 +92,41 @@ document.getElementById('reservation_form').addEventListener('submit', function(
     newWindow.document.close();
     newWindow.document.body.style.zoom = "100%"; // Browser automatically zooms in at 200%
 });
+document.getElementById('contact_form').addEventListener('submit', function(event) {
+  var isValid = true;
+  var errorMessages = '';
+  // Get Values 
+  var name = document.getElementById('name').value;
+  var phone = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+  var comment = document.getElementById('comment').value;
+
+// Validate Name 
+if (name.length <5){
+  errorMessages += 'Name must be 5 characters long.\n';
+  isValid = false;
+}
+  // Validate Phone (10 digits)
+  var phonePattern = /^[0-9]{10}$/; 
+  if (!phonePattern.test(phone)){
+    errorMessages += 'Phone must be 10 digits.\n';
+    isValid = false;
+  }
+// Validate Email 
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)){
+    errorMessages += 'Enter a valid email address.\n';
+    isValid = false;
+  }
+  // Validate Comments 
+  if (comment.length < 10){
+    errorMessages += 'Comments must be at least 10 characters long.\n';
+    isValid = false;
+  }
+  // if invalid 
+  if (!isValid){
+    alert (errorMessages);
+    event.preventDefault();
+  }
+});
+                                                         
