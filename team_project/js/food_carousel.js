1d0ec34152b2fb536dce38c13a40cf9354c17276
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-11 11:13:07 -0700

    Get image node

--- team_project/js/food_carousel.js
+++ team_project/js/food_carousel.js
@@ -3,3 +3,7 @@ image_source_array = ["images/thomas-marban-EHK-EH1SRzQ-unsplash.jpg", "images/k
     "images/mahmoud-fawzy-Dbx6-XZY6Dg-unsplash.jpg", "images/mahmoud-fawzy-MgvByrhYz24-unsplash.jpg", 
     "images/mahmoud-fawzy-YnVaHqgpPLA-unsplash.jpg"
 ]
+
+// Get image element
+let food_carousel_image = document.querySelector(".food_carousel img");
+
