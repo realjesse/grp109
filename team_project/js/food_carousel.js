aaa3b97c4509114b0df4732dd73e9987fc5f6d78
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-11 11:15:44 -0700

    Get left and right nodes on food_carousel

--- team_project/js/food_carousel.js
+++ team_project/js/food_carousel.js
@@ -4,12 +4,14 @@ image_source_array = ["images/thomas-marban-EHK-EH1SRzQ-unsplash.jpg", "images/k
     "images/mahmoud-fawzy-YnVaHqgpPLA-unsplash.jpg"
 ]
 
-// Get image element
+// Get image element, left and right element
 const food_carousel_image = document.querySelector(".food_carousel img");
+const left_carousel_div = document.querySelector(".food_carousel .left");
+const right_carousel_div = document.querySelector(".food_carousel .right");
 
 // Create index for images
-let index = 0
+let index = 0;
 
 function updateImage() {
-    food_carousel_image.src = image_source_array[index]
+    food_carousel_image.src = image_source_array[index];
 }
