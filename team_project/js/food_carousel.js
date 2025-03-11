f6acddcebe512241f56f60c6851780bc622cf3b2
realjesse <154276383+realjesse@users.noreply.github.com>
2025-03-11 11:10:12 -0700

    Create image_source_array and fill out

---
+++ team_project/js/food_carousel.js
@@ -0,0 +1,5 @@
+// Create food_carousel array
+image_source_array = ["images/thomas-marban-EHK-EH1SRzQ-unsplash.jpg", "images/kelly-visel-MIMgqOQLnOc-unsplash.jpg", 
+    "images/mahmoud-fawzy-Dbx6-XZY6Dg-unsplash.jpg", "images/mahmoud-fawzy-MgvByrhYz24-unsplash.jpg", 
+    "images/mahmoud-fawzy-YnVaHqgpPLA-unsplash.jpg"
+]
